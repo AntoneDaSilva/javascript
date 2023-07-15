4587dbeaa3908ff55fcc3d66f850569caa72de46
AntoneDaSilva <131554000+AntoneDaSilva@users.noreply.github.com>
2023-07-15 14:02:55 -0300

    teste

--- ex006/javascri.js
+++ ex006/javascri.js
@@ -1,12 +1,26 @@
-class pessoal{
-    constructor(pnome){
+class Pessoa{
+    canal='CFB Cursos'
+    constructor(pnome,pidade){
         this.nome = pnome
+        this.idade = pidade
+
+    }
+    getNome(){
+        return this.nome
+    }
+    getIdade(){
+        return this.idade
+    }
+    setNome(nome){
+        this.nome=nome
+    }
+    setidade(idade){
+        this.idade=idade
+    }
+    info(){
+        console.log(`nome: ${this.nome} `)
+        console.log(`idade: ${this.idade}`)
+        console.log('----------------------')
     }
 }
-
-let p1 = new pessoal('Antony')
-let p2 = new pessoal('Bruno')
-let p3 = new pessoal('Rodolfo')
-console.log(p1.nome)
-console.log(p2.nome)
-console.log(p3.nome)
+const btn_add = document.querySelector('#btn_add')
