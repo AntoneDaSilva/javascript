9c3c0e7dbf8925b8e22e0e69f5bdbbfe2b2842c7
AntoneDaSilva <131554000+AntoneDaSilva@users.noreply.github.com>
2023-07-17 16:14:54 -0300

    objetos

--- ex006/javascri.js
+++ ex006/javascri.js
@@ -1,49 +1,51 @@
-class Pessoa{
-    canal='CFB Cursos'
-    constructor(pnome,pidade){
-        this.nome = pnome
-        this.idade = pidade
-
-    }
-    getNome(){
-        return this.nome
-    }
-    getIdade(){
-        return this.idade
-    }
-    setNome(nome){
-        this.nome=nome
-    }
-    setidade(idade){
-        this.idade=idade
-    }
-    info(){
-        console.log(`nome: ${this.nome} `)
-        console.log(`idade: ${this.idade}`)
-        console.log('----------------------')
-    }
-}
-let pessoa = []
-
-const btn_add = document.querySelector('#btn_add')
-const res = document.querySelector('.res')
-
-const addPessoa = ()=>{
-    pessoa.map((p)=>{
-        const div = document.createElement('div')
-        div.setAttribute('class', 'pessoa')
-        div.innerHTML = `nome: ${p.getNome()}<br>${p.getIdade()}` 
-        res.appendChild(div)
-    })
+const pessoa = {
+    nome:'Antony'
 }
 
-btn_add.addEventListener('click', (evt)=>{
-    const nome = document.querySelector('#f_name')
-    const idade = document.querySelector('#f_idade')
-    const personalList = new Pessoa(nome.value, idade.value)
-    pessoa.push(personalList)
-    nome.value = ''
-    idade.value = ''
-    nome.focus()
-    addPessoa()
-})
+
+//  function Pessoa(pnome,pidade){
+//         this.nome = pnome,
+//         this.idade = pidade,
+
+//     this.getNome = function(){
+//         return this.nome
+//     },
+//     this.getIdade = function(){
+//         return this.idade
+//     },
+//     this.setNome = function(nome){
+//         this.nome=nome
+//     },
+//     this.setidade = function(idade){
+//         this.idade=idade
+//     },
+//     this.info =  function(){
+//         console.log(`nome: ${this.nome} `)
+//         console.log(`idade: ${this.idade}`)
+//         console.log('----------------------')
+//     }
+// }
+// let pessoa = []
+
+// const btn_add = document.querySelector('#btn_add')
+// const res = document.querySelector('.res')
+
+// const addPessoa = ()=>{
+//     pessoa.map((p)=>{
+//         const div = document.createElement('div')
+//         div.setAttribute('class', 'pessoa')
+//         div.innerHTML = `nome: ${p.getNome()}<br>idade: ${p.getIdade()}` 
+//         res.appendChild(div)
+//     })
+// }
+
+// btn_add.addEventListener('click', (evt)=>{
+//     const nome = document.querySelector('#f_name')
+//     const idade = document.querySelector('#f_idade')
+//     const personalList = new Pessoa(nome.value, idade.value)
+//     pessoa.push(personalList)
+//     nome.value = ''
+//     idade.value = ''
+//     nome.focus()
+//     addPessoa()
+// })
