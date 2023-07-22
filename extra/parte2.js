e1f0df85afae45cc07147ba9913237241cc0c7d9
AntoneDaSilva <131554000+AntoneDaSilva@users.noreply.github.com>
2023-07-22 03:11:22 -0300

    Update parte2.js

--- extra/parte2.js
+++ extra/parte2.js
@@ -1,15 +1,17 @@
-const par = (a) => {
-    if(a % 2 == 0){
-        return a.toString().slice(0, 2)
-    }
-}
+// const par = (a) => {
+//     if(a % 2 == 0){
+//         return a.toString().slice(0, 2)
+//     }else{
+//         return 'Harry Potter'
+//     }
+// }
 
-console.log(par(100))
-// const partes = (str) => 
-//      str.length < 3 ? str :  str.slice(0, 3) + str.slice(-3)
+// console.log(par(25))
+const partes = (str) => 
+     str.length < 3 ? str :  str.slice(0, 3) + str.slice(-3)
 
 
-// console.log(partes('acbs'))
-// console.log(partes('abcdfegh'))
-// console.log(partes('abc173879374655376'))
-// console.log(partes('ac'))
+console.log(partes('acbs'))
+console.log(partes('abcdfegh'))
+console.log(partes('abc173879374655376'))
+console.log(partes('ac'))
