fb89c7245c90f2422ab3b197e899cb23851ac49a
AntoneDaSilva <131554000+AntoneDaSilva@users.noreply.github.com>
2023-07-24 02:35:54 -0300

    sla

--- ex009/js.js
+++ ex009/js.js
@@ -1,16 +1,22 @@
-const pessoa = {
-    nome:'Bruni',
-    canal: 'CFB Cursos',
-    curso: 'JavaScript',
-    aulas:{
-        aula01: 'introdução',
-        aula02: 'Variáveis',
-        aula03: 'Condicionais' 
+class Npc{
+    constructor(energia){
+        this.energia = energia
+        this.alerta = false
     }
-}
-const obj = '{"nome":"Bruni","canal":"CFB Cursos","curso":"JavaScript","aulas":{"aula01":"introdução","aula02":"Variáveis","aula03":"Condicionais"}}'
+    info = () => {
+        console.log(`Energia: ${this.energia}`)
+        console.log(this.alerta ? 'Sim' : 'Não')
+    }
+} 
 
-const obj_json = JSON.parse(obj)
-const str_json = JSON.stringify(pessoa)
-console.log(str_json)
-console.log(obj_json)
+const npc1 = new Npc(100)
+const npc2 = new Npc(80)
+const npc3 = new Npc(30)
+
+npc1.alerta = true
+npc2.alerta = true
+npc1.alerta = true
+
+npc1.info()
+npc2.info()
+npc3.info()
