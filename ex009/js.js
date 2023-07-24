848872d0de0daaee58a3273c4015fd325c7a6aec
AntoneDaSilva <131554000+AntoneDaSilva@users.noreply.github.com>
2023-07-24 04:06:08 -0300

    mudanca

--- ex009/js.js
+++ ex009/js.js
@@ -1,11 +1,16 @@
 class Npc{
+    static alerta = false
     constructor(energia){
         this.energia = energia
-        this.alerta = false
     }
     info = () => {
         console.log(`Energia: ${this.energia}`)
-        console.log(this.alerta ? 'Sim' : 'Não')
+        console.log(`Alerta: ${(Npc.alerta ? 'Sim' : 'Não')}`)
+        console.log('------------------------')
+    }
+
+    static Alerta = () => {
+        Npc.alerta = true
     }
 } 
 
@@ -13,9 +18,7 @@ const npc1 = new Npc(100)
 const npc2 = new Npc(80)
 const npc3 = new Npc(30)
 
-npc1.alerta = true
-npc2.alerta = true
-npc1.alerta = true
+Npc.Alerta() 
 
 npc1.info()
 npc2.info()
